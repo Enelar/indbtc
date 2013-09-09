5b00c9f95eb9360574d0fd7343ca993ba60112be
root <root@indbtc.com>
2013-09-09 13:31:27 +0400

    Ready for use comtube sms api

--- api/bitcoin.php
+++ api/bitcoin.php
@@ -1,29 +1,6 @@
 <?php
 
-function http_request($uri, $time_out = 10, $headers = 0)
-{
-    // Initializing
-    $ch = curl_init();
-
-    // Set URI
-    curl_setopt($ch, CURLOPT_URL, trim($uri));
-
-    curl_setopt($ch, CURLOPT_HEADER, $headers);
-
-    // 1 - if output is not needed on the browser
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    // Time-out in seconds
-    curl_setopt($ch, CURLOPT_TIMEOUT, $time_out);
-
-    // Executing
-    $result = curl_exec($ch);
-
-    // Closing the channel
-    curl_close($ch);
-
-    return $result;
-}
+include_once('lib/curl.php');
 
 class bitcoin extends api
 { 
